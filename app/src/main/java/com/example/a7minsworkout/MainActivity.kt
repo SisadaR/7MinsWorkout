4bcd1317000e740b0bda58ffeb2f6f6c69c78830
Sisada R <kong.sisada@gmail.com>
2021-01-05 03:25:06 +0700

    prepare main menu

--- app/src/main/java/com/example/a7minsworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minsworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.a7minsworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<LinearLayout>(R.id.llStart).setOnClickListener {
+            Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
+        }
     }
 }
